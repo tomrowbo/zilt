38d7cbb772a293c6aadef6dabb1c7ea597853e0f
c1847017 <yiallouridess@cardiff.ac.uk>
2024-10-13 11:12:19 +0300

    Remove unnecessary code

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -18,23 +18,10 @@ export default function Header({ mode = 'dark' }: {
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
-
-            {/* Desktop menu links */}
-            <ul className="flex grow justify-start flex-wrap items-center">
-              <li>
-                <Link href="/ramp" className="font-medium text-slate-800 hover:text-blue-600 dark:text-slate-400 dark:hover:text-blue-600 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Ramp</Link>
-              </li>
-
-             
-            </ul>
-
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link href="/signin" className="font-medium text-slate-800 hover:text-blue-600 dark:text-slate-400 dark:hover:text-blue-600 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Sign in</Link>
-              </li>
-              <li>
-                <Link href="/request-demo" className="font-medium text-blue-600 dark:text-slate-300 dark:hover:text-white px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out group">
+                <Link href="/ramp" className="font-medium text-blue-600 dark:text-slate-300 dark:hover:text-white px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out group">
                   Get Started<span className="tracking-normal text-blue-600 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
                 </Link>
               </li>
