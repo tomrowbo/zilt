9d12f8f4eedaae7cba40fc39e4aa1ce82376f800
c1847017 <yiallouridess@cardiff.ac.uk>
2024-10-13 11:56:53 +0300

    Add additional env variables

--- utils/mpesa.ts
+++ utils/mpesa.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-//TODO: Extract to .env
-const consumerKey = 'tsMS93qRb8PGq61Prs5e2afzxEfnDnblmjduLkJG1wJDKev7';
-const consumerSecret = 'eEeByht1SY6FejjONGOA5iTYP6PuN3OkAsCqkWDYh2kzqxQpdz3LitnumjErrP2G';
+const consumerKey = process.env.MPESA_CONSUMER_KEY;
+const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+
+if (!consumerKey || !consumerSecret) {
+  throw new Error('MPESA_CONSUMER_KEY or MPESA_CONSUMER_SECRET environment variable is not set');
+}
 
 export async function getAccessToken(): Promise<string> {
   const credentials = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
